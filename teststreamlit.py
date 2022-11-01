6892c5dcb415b6382092ec782dc593bc78c96213
nielshansen1 <114566358+nielshansen1@users.noreply.github.com>
2022-11-01 13:34:44 +0100

    Add files via upload

---
+++ teststreamlit.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+
+# In[6]:
+
+
+st.set_page_config(
+    page_title="TEST",
+    layout="wide")
+
+
+# In[7]:
+
+
+#dashboard title
+st.title("Streamlit Test")
+
